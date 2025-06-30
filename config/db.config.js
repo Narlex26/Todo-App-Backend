a9a20cadbb05e2e680509d1dcfa329f84059d621
Johnn81100 <103576060+Johnn81100@users.noreply.github.com>
2025-06-30 11:31:07 +0200

    docker/setup_docker: ajout de la fonction app.listen() dans app.js, modification  des paramètres db, mise a jour version dans package-lock.json

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: process.env.DB_HOST || "localhost",
   USER: process.env.DB_USER || "root",
-  PASSWORD: process.env.DB_PASSWORD || "",
+  PASSWORD: process.env.DB_PASSWORD || "root",
   DB: process.env.DB_NAME || "todo_app_db",
   dialect: "mysql",
   pool: {
