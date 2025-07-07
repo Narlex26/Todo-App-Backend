49f49eca5ed896b21f1b70f209d55361151dc58e
Alexandre Boyer <alexandreboyer39@gmail.com>
2025-07-07 12:05:42 +0200

    Improve CORS error logging to include the rejected origin

--- app.js
+++ app.js
@@ -31,8 +31,7 @@ const corsOptions = {
     if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      console.log(`CORS: Origine refusée - ${origin}`);
-      callback(new Error('Non autorisé par CORS'));
+      callback(new Error(`Non autorisé par CORS - ${origin}`));
     }
   },
   credentials: true,
